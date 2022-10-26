11a31f84d63898643dd99832eb33fc455377491b
amanchhaparia <amanchhaparia125@gmail.com>
2022-10-26 12:50:09 +0530

    Comparison of encoder of KFR and buddy audio container

--- tests/Interface/core/AudioContainerTest.cpp
+++ tests/Interface/core/AudioContainerTest.cpp
@@ -22,11 +22,16 @@
 
 #include "Interface/buddy/dap/AudioContainer.h"
 #include <iostream>
+#include <kfr/base.hpp>
+#include <kfr/dft.hpp>
+#include <kfr/dsp.hpp>
+#include <kfr/io.hpp>
 
 using namespace std;
+using namespace kfr;
 
 int main() {
-  dap::Audio<float, 1> aud("../../../../tests/Interface/core/NASA_Mars.wav");
+  dap::Audio<float, 1> aud("../../tests/Interface/core/NASA_Mars.wav");
   auto &audioFile = aud.getAudioFile();
   // CHECK: 1
   fprintf(stderr, "%u\n", audioFile.getNumChannels());
@@ -36,6 +41,56 @@ int main() {
   fprintf(stderr, "%u\n", audioFile.getNumSamplesPerChannel());
   // CHECK: 100000
   fprintf(stderr, "%u\n", audioFile.getSampleRate());
+
+  float data[] =  {0.05, 2.06, 4.05, 100.06, 20000.888, -0.05, -2.06, -4.05, -100.06, -20000.888};
+  int size = sizeof(data)/sizeof(data[0]);
+
+  univector<float> data_kfr(data, data+size);
+
+  cout<<"---------------------RAW DATA--------------------------\n";
+  for(int i=0; i<size; i++){
+    cout<<data_kfr[i]<<", ";
+  }
+  cout<<"\n \n";
+
+  // Save data using Buddy Audio Container
+  dap:: Audio<float, 1>output;
+  output.getAudioFile().setBitDepth(24);
+  output.getAudioFile().setSampleRate(100000);
+  output.getAudioFile().numChannels = 1;
+  output.getAudioFile().numSamples = size;
+  output.getAudioFile().setAudioBuffer(data);
+  
+  output.save("Test_Output.wav");
+
+  // Read and output the encoded values.
+  dap:: Audio<float, 1> aud_read("Test_Output.wav");
+  auto &audioFileRead = aud_read.getAudioFile();
+
+  cout<<"-----------BUDDY AUDIO CONTAINER ENCODED---------------\n";
+  for(int i=0; i<size; i++){
+    cout<<audioFileRead.samples.get()[i]<<", ";
+  }
+  cout<<"\n \n";
+  
+  // Save data using KFR Audio writer
+  audio_writer_wav<float> writer(
+        open_file_for_writing("Test_Output_kfr.wav"),
+        audio_format{1 /* channel */, audio_sample_type::i24,
+                    100000 /* sample rate */});
+
+  writer.write(data_kfr.data(), data_kfr.size());
+  writer.close();
+
+  // Read and output the KFR encoded values.
+  dap:: Audio<float, 1> kfr_aud_read("Test_Output_kfr.wav");
+  auto &kfrAudioRead = kfr_aud_read.getAudioFile();
+
+  cout<<"-----------KFR AUDIO ENCODED---------------\n";
+  for(int i=0; i<size; i++){
+    cout<<kfrAudioRead.samples.get()[i]<<", ";
+  }
+  cout<<"\n";
 
   return 0;
 }
